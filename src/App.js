018313bc783c8d992c43d41879569a6c5c0ffda3
Julianpse <julianpse@gmail.com>
2018-06-15 20:52:50 -0500

    form data showing

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ injectTapEventPlugin();
 
 class App extends Component {
 
+  state = {
+    fields: {}
+  };
+
   onSubmit = (fields) =>  {
     console.log('Here are the submitted fields', fields);
+    this.setState({fields});
   };
 
 
@@ -19,6 +24,14 @@ class App extends Component {
           <h1 className="App-title">Contact Form</h1>
         </header>
         <Form onSubmit={fields => this.onSubmit(fields)} />
+        <br/>
+          <h1>Contacts</h1>
+          <h3>First Name: {this.state.fields.firstName}</h3>
+          <h3>Last Name: {this.state.fields.lastName}</h3>
+          <h3>Email: {this.state.fields.email}</h3>
+          <h3>City: {this.state.fields.city}</h3>
+          <h3>State: {this.state.fields.usState}</h3>
+          <h3>Zip Code: {this.state.fields.zipCode}</h3>
       </div>
     );
   }
